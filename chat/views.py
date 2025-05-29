0b1d8a613c88eb4e986393d9d4e5c8a9163dcfcb
Rahul-Raval-2912 <rahul9raval@gmail.com>
2025-05-29 20:33:50 +0530

    strat adding future

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
+from raven_project.chat.forms import RegisterForm
 
 def login_view(request):
-    return render(request, 'chat/login.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # Create this URL later
+        else:
+            return HttpResponse("Invalid username or password.")
+    return render(request, "chat/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
 def register_view(request):
-    return render(request, 'chat/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # You can create login page later
+    else:
+        form = RegisterForm()
+    return render(request, 'chat/register.html', {'form': form})
